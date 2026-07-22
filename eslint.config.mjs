19c7c68bd6580e2fdabf3b51152851bd8a9c033b
Xykyma <nikitapelevin@gmail.com>
2026-07-23 00:31:05 +0300

    chore: exclude private/ from eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint';
 import reactHooks from 'eslint-plugin-react-hooks';
 
 export default tseslint.config(
-  { ignores: ['out/**', 'dist/**', 'release/**', 'node_modules/**', 'docs/**', 'Design backup/**'] },
+  { ignores: ['out/**', 'dist/**', 'release/**', 'node_modules/**', 'docs/**', 'Design backup/**', 'private/**'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
